f8c8f58c8370748a4ab908ce2f400138b2dd0734
Pawan Maniyar <maniyar.pawan26@gmail.com>
2023-07-29 23:58:27 +0530

    Demo app for circle cicd

---
+++ app/src/main/java/com/pawan/circlecicdapp/SecondActivity.kt
@@ -0,0 +1,20 @@
+package com.pawan.circlecicdapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pawan.circlecicdapp.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBackSecondActivity.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
